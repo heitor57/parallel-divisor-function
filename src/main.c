396219e3b2e9d2b95b884ff0a90ebfd203d3da98
heitor57 <heitorwerneck@hotmail.com>
2021-10-27 20:53:27 -0300

    fix: all serial divisor function

--- src/main.c
+++ src/main.c
@@ -1,50 +1,87 @@
+#include <argp.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int file_num_lines(FILE* f) {
-    char c;
-    int lines = 0;
-    while (!feof(f)) {
-        c = fgetc(f);
-        if (c == '\n') {
-            lines++;
-        }
+#include "serial_divisor_function.h"
+
+int file_num_lines(FILE *f) {
+  char c;
+  int lines = 0;
+  while (!feof(f)) {
+    c = fgetc(f);
+    if (c == '\n') {
+      lines++;
     }
-    fseek(f, 0, SEEK_SET);
-    return lines;
+  }
+  fseek(f, 0, SEEK_SET);
+  return lines;
 }
 
-int get_max(int* vector, int num_elements) {
-    int max = vector[0];
-    for (int i = 1; i < num_elements; i++) {
-        if (vector[i] > max) {
-            max = vector[i];
-        }
+int get_max(int *vector, int num_elements) {
+
+  int max = vector[0];
+  for (int i = 1; i < num_elements; i++) {
+    if (vector[i] > max) {
+      max = vector[i];
     }
-    return max;
+  }
+  return max;
 }
 
-int main(int argc, char** argv) {
-    int num, i, num_integers;
-    char fname[100] = "entrada.txt";
-    FILE* file = fopen(fname, "r");
-    if (file == NULL) {
-        fprintf(stderr, "Unable to open file %s\n", fname);
-        return 1;
+int *mask_vector_to_int_vector(bool *v, int size, int* int_vector_size) {
+  int num_true = 0;
+  for (int i = 0; i < size; i++) {
+    if (v[i] == true) {
+      num_true++;
     }
-    int num_lines = file_num_lines(file);
-    int* integers = malloc(sizeof(int) * num_lines);
-    i = 0;
-    while (fscanf(file, "%d", &num) > 0) {
-        integers[i] = num;
-        printf("%d\n", integers[i]);
-        i++;
+  }
+  *int_vector_size = num_true;
+  int *iv = malloc(*int_vector_size * sizeof(int));
+  int j = 0;
+  for (int i = 0; i < size; i++) {
+    if (v[i] == true) {
+      iv[j] = i;
+      j++;
     }
-    num_integers = i;
-    printf("%d %d\n", num_integers, num_lines);
-    int max_number = get_max(integers, num_integers);
+  }
+  return iv;
+}
 
-    fclose(file);
-    free(integers);
-    return 0;
+int main(int argc, char **argv) {
+  int num, i, num_integers;
+  char fname[100] = "entrada.txt";
+  FILE *file = fopen(fname, "r");
+  if (file == NULL) {
+    fprintf(stderr, "Unable to open file %s\n", fname);
+    return 1;
+  }
+  int num_lines = file_num_lines(file);
+  int *integers = malloc(sizeof(int) * num_lines);
+  i = 0;
+  while (fscanf(file, "%d", &num) > 0) {
+    integers[i] = num;
+    /*printf("%d\n", integers[i]);*/
+    i++;
+  }
+  num_integers = i;
+  printf("%d integers, %d lines\n", num_integers, num_lines);
+  int max_number = get_max(integers, num_integers);
+  printf("maximum number: %d\n", max_number);
+  int primes_mask_size;
+  bool *primes_mask = serial_divisor_function_sieve_of_eratosthenes(
+      max_number, &primes_mask_size);
+  int num_primes;
+  int *primes = mask_vector_to_int_vector(primes_mask, primes_mask_size,&num_primes);
+  int num_divisors;
+  for (i = 0; i < num_integers; i++) {
+    num_divisors =
+        serial_divisor_function_df(integers[i], primes, num_primes);
+    printf("integer: %d, num_divisors: %d\n", integers[i],num_divisors);
+  }
+  fclose(file);
+  free(integers);
+  free(primes);
+  free(primes_mask);
+  return 0;
 }
--- src/serial_divisor_function.c
+++ src/serial_divisor_function.c
@@ -1,30 +1,44 @@
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-bool *serial_divisor_function_sieve_of_eratosthenes(int limit) {
-    bool *prime = malloc((limit + 1) * sizeof(bool));
-    memset(prime, true, (limit + 1) * sizeof(bool));
-
-    for (int i = 2; i * i <= limit; i++) {
-        if (prime[i] == true) {
-            for (int j = i * i; j <= limit; j += i) prime[j] = false;
-        }
+bool *serial_divisor_function_sieve_of_eratosthenes(int limit,
+                                                    int *primes_mask_size) {
+  *primes_mask_size = limit + 1;
+  bool *primes = malloc(*primes_mask_size * sizeof(bool));
+  memset(primes, true, *primes_mask_size * sizeof(bool));
+  primes[0] = false;
+  for (int i = 2; i * i <= limit; i++) {
+    if (primes[i] == true) {
+      for (int j = i * i; j <= limit; j += i)
+        primes[j] = false;
     }
-    return prime;
+  }
+  return primes;
 }
-
+/**
+ * @brief
+ *
+ * @param number
+ * @param primes
+ * @param num_primes
+ * @return int
+ */
 int serial_divisor_function_df(int number, int *primes, int num_primes) {
-    int prime, count, num_divisors = 1;
-    for (int i = 0; i <= num_primes; i++) {
-        prime = primes[i];
-        count = 0;
-        if (number % prime == 0) {
-            while (number % prime == 0) {
-                number = number / prime;
-                count++;
-            }
-            num_divisors = num_divisors * (count + 1);
-        }
+  int prime, count, num_divisors = 1;
+  for (int i = 1; i < num_primes; i++) {
+    prime = primes[i];
+    /*printf("%d/%d prime %d\n", i, num_primes, prime);*/
+    /*fflush(stdout);*/
+    count = 0;
+    if (number % prime == 0) {
+      while (number % prime == 0) {
+        number = number / prime;
+        count++;
+      }
+      num_divisors = num_divisors * (count + 1);
     }
-    return num_divisors;
+  }
+  return num_divisors;
 }
