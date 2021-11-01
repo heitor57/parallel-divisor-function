c91550f4da8433f59edc0ae7988c2a609d61c5d1
heitor57 <heitorwerneck@hotmail.com>
2021-10-31 21:36:21 -0300

    update: docs and utils.h

--- src/utils.h
+++ src/utils.h
@@ -2,7 +2,6 @@
 #define UTILS_H
 #include <stdbool.h>
 #include <stdio.h>
-int *mask_vector_to_int_vector(bool *v, int size, int *int_vector_size);
 int get_max(int *vector, int num_elements);
 int file_num_lines(FILE *f);
 int *initial_setup(int argc, char **argv, int *num_integers);
