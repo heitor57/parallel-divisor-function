7f2bfd6b5fa0aa039f0c0d195a4f04160b2f5ed0
heitor57 <heitorwerneck@hotmail.com>
2021-10-31 23:38:12 -0300

    update

--- test/print.py
+++ test/print.py
@@ -1,11 +1,11 @@
 for base in ["t1e4", "t1e5", "t1e6", "t1e7"]:
     for i in ["serial", "parallel"]:
         if i == "parallel":
-            for j in range(2, 25):
+            for j in [2, 3, 4, 8, 16, 24]:
                 with open(f"data/{base}_stdout_{j}_{i}.txt", "r") as f:
                     st = f.read()
-                    print(float(st[28:][:-1]))
+                    print(f"data/{base}_stdout_{j}_{i}.txt", float(st[28:][:-1]))
         else:
             with open(f"data/{base}_stdout_{i}.txt", "r") as f:
                 st = f.read()
-                print(float(st[28:][:-1]))
+                print(f"data/{base}_stdout_{i}.txt", float(st[28:][:-1]))
