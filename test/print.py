aff70fa5c446a4efda2b792dfd4ed6d63cc33e19
heitor57 <heitorwerneck@hotmail.com>
2021-12-13 00:24:55 -0300

    update requirements, print and cmakelists

--- test/print.py
+++ test/print.py
@@ -1,7 +1,7 @@
 for base in ["t1e4", "t1e5", "t1e6", "t1e7"]:
     for i in ["serial", "parallel"]:
         if i == "parallel":
-            for j in [2, 3, 4, 8, 16, 24]:
+            for j in [7, 10, 13]:
                 with open(f"data/{base}_stdout_{j}_{i}.txt", "r") as f:
                     st = f.read()
                     print(f"data/{base}_stdout_{j}_{i}.txt", float(st[28:][:-1]))
@@ -9,3 +9,9 @@ for base in ["t1e4", "t1e5", "t1e6", "t1e7"]:
             with open(f"data/{base}_stdout_{i}.txt", "r") as f:
                 st = f.read()
                 print(f"data/{base}_stdout_{i}.txt", float(st[28:][:-1]))
+    
+    for i in [2, 3, 4]:
+        fname = f"data/{base}_stdout_{i}_parallel_hybrid.txt"
+        with open(fname, "r") as f:
+            st = f.read()
+            print(fname, float(st[28:][:-1]))
