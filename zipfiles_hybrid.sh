74981e5e06164cc5ec1564b2fa7b45b2b31cf182
heitor57 <heitorwerneck@hotmail.com>
2021-12-13 21:47:39 -0300

    fix zip files and doc update

--- zipfiles_hybrid.sh
+++ zipfiles_hybrid.sh
@@ -2,6 +2,6 @@
 
 cp ./dochybrid/doc.pdf documentation.pdf
 
-zip -R HeitorWerneckTP3.zip README.md Makefile Dockerfile CMakeLists.txt .dockerignore documentation.pdf test/ src/
+zip -R HeitorWerneckTP3.zip README.md Makefile Dockerfile CMakeLists.txt .dockerignore documentation.pdf test/* src/*
 
 rm documentation.pdf
