c963f7889c0c9f2830eef115f01cf675d422ef7a
heitor57 <heitorwerneck@hotmail.com>
2021-12-13 21:33:33 -0300

    feat: zip files script

---
+++ zipfiles_hybrid.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp ./dochybrid/doc.pdf documentation.pdf
+
+zip -R HeitorWerneckTP3.zip README.md Makefile Dockerfile CMakeLists.txt .dockerignore documentation.pdf test/ src/
+
+rm documentation.pdf
